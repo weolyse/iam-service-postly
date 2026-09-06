cafefa155a76ad03960f4d3c3819bfe2fa739b19
Abdunur <weolyse@MacBook-Air-Abdunur.local>
2026-09-06 13:25:52 +0500

    feat: handle post NotFoundException

---
+++ src/main/java/com/postly/iam_service/advice/CommonControllerAdvice.java
@@ -0,0 +1,53 @@
+package com.postly.iam_service.advice;
+
+import com.postly.iam_service.model.constant.ApiConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Slf4j
+@ControllerAdvice
+public class CommonControllerAdvice {
+
+    @ExceptionHandler
+    @ResponseBody
+    protected ResponseEntity<String> handleNotFoundException(Exception ex) {
+        logStackTrace(ex);
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    protected void logStackTrace(Exception ex) {
+        StringBuilder stackTrace = new StringBuilder();
+
+        stackTrace.append(ApiConstants.ANSI_RED);
+
+        stackTrace.append(ex.getMessage()).append(ApiConstants.BREAK_LINE);
+
+        if (Objects.nonNull(ex.getCause())) {
+            stackTrace.append(ex.getCause().getMessage()).append(ApiConstants.BREAK_LINE);
+        }
+
+        Arrays.stream(ex.getStackTrace())
+                .filter(st -> st.getClassName().startsWith(ApiConstants.TIME_ZONE_PACKAGE_NAME))
+                .forEach(st -> stackTrace
+                        .append(st.getClassName())
+                        .append(".")
+                        .append(st.getMethodName())
+                        .append(" (")
+                        .append(st.getLineNumber())
+                        .append(") ")
+                );
+
+        log.error(stackTrace.append(ApiConstants.ANSI_WHITE).toString());
+    }
+}
---
+++ src/main/java/com/postly/iam_service/model/constant/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.postly.iam_service.model.constant;
+
+public class ApiConstants {
+
+    public static final String UNDEFINED = "undefined";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String BREAK_LINE = "\n";
+    public static final String TIME_ZONE_PACKAGE_NAME = "java.time.zone";
+}
